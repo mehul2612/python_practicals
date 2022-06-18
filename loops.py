24bbffafe45df36ad476b015ab4797347dea0196
mehul2612 <87797568+mehul2612@users.noreply.github.com>
2022-06-18 10:13:38 +0530

    Update loops.py

---
+++ loops.py
@@ -0,0 +1,44 @@
+# Program to find the sum of all numbers stored in a list
+
+# List of numbers
+numbers = [6, 5, 3, 8, 4, 2, 5, 4, 11]
+
+# variable to store the sum
+sum = 0
+
+# iterate over the list
+for val in numbers:
+    sum = sum+val
+
+print("The sum is", sum)
+
+# Program to iterate through a list using indexing
+
+genre = ['pop', 'rock', 'jazz']
+
+# iterate over the list using index
+for i in range(len(genre)):
+    print("I like", genre[i])
+
+
+#while loop
+
+    # Program to add natural
+# numbers up to 
+# sum = 1+2+3+...+n
+
+# To take input from the user,
+# n = int(input("Enter n: "))
+
+n = 10
+
+# initialize sum and counter
+sum = 0
+i = 1
+
+while i <= n:
+    sum = sum + i
+    i = i+1    # update counter
+
+# print the sum
+print("The sum is", sum)
